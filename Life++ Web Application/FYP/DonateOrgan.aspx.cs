f3cf306f6456c322ef28f1e7dbe0f8d3ce61ae21
Sachin Antony Edwin Earayil <38481988+sacedwin@users.noreply.github.com>
2020-01-20 23:22:45 +1100

    Update DonateOrgan.aspx.cs

--- Life++ Web Application/FYP/DonateOrgan.aspx.cs
+++ Life++ Web Application/FYP/DonateOrgan.aspx.cs
@@ -320,7 +320,7 @@ public partial class DonateOrgan : System.Web.UI.Page
 		//string from = origin.Text;
 		//string to = destination.Text;
 
-		string url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origin + "&destinations=" + destination + "&key=AIzaSyCN-DotpprhnjmZrnqpX1Sa-gZK8freYkc";
+		string url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + origin + "&destinations=" + destination + "&key= /*ENTER THE KEY HERE*/ ";
 
 		string requesturl = url.Replace('#', ' ');
 		int content = fileGetContents(requesturl);
